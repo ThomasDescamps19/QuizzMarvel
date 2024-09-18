d2a3a1cedf9820ec22caa942eb797f9b4ea5c812
ThomasDescamps19 <thomasdescamps19@gmail.com>
2024-09-18 17:22:48 +0200

    logout

--- src/components/Welcome/index.js
+++ src/components/Welcome/index.js
@@ -1,10 +1,38 @@
-import React from "react";
+import React, { useContext, useState, useEffect } from "react";
+import Logout from "../Logout";
+import Quiz from "../Quiz";
+import { FirebaseContext } from "../Firebase";
+import { useNavigate } from "react-router-dom"; // Importation de useNavigate
 
 const Welcome = () => {
-  return (
-    <div className="quiz-bg">
-      <div className="container">Welcome</div>
-    </div>
+  const navigate = useNavigate();
+  const [userSession, setUserSession] = useState(null);
+  const firebase = useContext(FirebaseContext);
+
+  useEffect(() => {
+    let listener = firebase.auth.onAuthStateChanged((user) => {
+      user ? setUserSession(user) : navigate("/");
+    });
+
+    return () => {
+      listener();
+    };
+  }, []);
+
+  return userSession === null ? (
+    <>
+      <div className="loader"></div>
+      <p className="loaderText">...</p>
+    </>
+  ) : (
+    <>
+      <div className="quiz-bg">
+        <div className="container">
+          <Logout />
+          <Quiz />
+        </div>
+      </div>
+    </>
   );
 };
 
